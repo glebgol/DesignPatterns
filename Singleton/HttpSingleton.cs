2095382ae694bf121415d6bc662b6c29ec5bf584
Gleb Golovnev <glebgolovnev200@gmail.com>
2022-10-25 00:54:13 +0300

    Added http get method

--- Singleton/HttpSingleton.cs
+++ Singleton/HttpSingleton.cs
@@ -2,5 +2,33 @@
 {
     public class HttpSingleton
     {
+        private static HttpSingleton instance;
+        private HttpClient _httpClient;
+        private string _baseAdress;
+
+        private HttpSingleton(string baseAdress)
+        {
+            _baseAdress = baseAdress;
+            _httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri(baseAdress)
+            };
+        }
+
+        public static HttpSingleton GetInstance(string baseAdress)
+        {
+            if (instance == null)
+            {
+                instance = new HttpSingleton(baseAdress);
+            }
+            return instance;
+        }
+
+        public Task<HttpResponseMessage> Get(string baseAdress)
+        {
+            var httpInstance = GetInstance(baseAdress);
+            var result = httpInstance._httpClient.GetAsync(baseAdress);
+            return result;
+        }
     }
 }
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Singleton;
+
+class Program
+{
+    private static string baseAdress = "https://localhost:7248/api/products";
+    public static void Main()
+    {
+        var httpSingleton = HttpSingleton.GetInstance(baseAdress);
+        var httpResponse = httpSingleton.Get(baseAdress);
+        var result = httpResponse.Result;
+        Console.WriteLine(result);
+    }
+}
