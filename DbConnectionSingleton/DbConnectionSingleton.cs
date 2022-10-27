3efc38bc244b504bf72669d0e396e3149f7aa02d
Gleb Golovnev <glebgolovnev200@gmail.com>
2022-10-27 18:29:53 +0300

    Added new project DbConnectionSingleton

---
+++ DbConnectionSingleton/DbConnectionSingleton.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.SqlClient;
+
+namespace DbConnectionSingleton
+{
+    public class DbConnectionSingleton
+    {
+        private readonly string connectionString = "Server=GLEBGOL;Database=FridgeDb;Trusted_Connection=True;";
+        private readonly SqlConnection sqlConnection = new SqlConnection();
+    }
+}
