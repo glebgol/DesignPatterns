309ceb8f218148d22109fecae678962b30c21a90
Gleb Golovnev <glebgolovnev2004@gmail.com>
2022-12-02 00:40:31 +0300

    Fix Singleton

---
+++ Singleton/ThreadSafeSingleton.cs
@@ -0,0 +1,26 @@
+﻿namespace Singleton
+{
+    class ThreadSafeSingleton
+    {
+        private ThreadSafeSingleton() { }
+
+        private static ThreadSafeSingleton _instance;
+
+        private static readonly object _lock = new object();
+
+        public static ThreadSafeSingleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        _instance ??= new ThreadSafeSingleton();
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
